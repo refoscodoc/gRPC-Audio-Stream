0cff14876b58bb19247e3706d6fa98ad16541181
refoscodoc <mezzasegale@gmail.com>
2024-09-16 19:40:05 +0200

    Add cscore library folder

---
+++ cscore-master/CSCore.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Wenn ComVisible auf "false" festgelegt wird, sind die Typen innerhalb dieser Assembly für
+// COM-Komponenten unsichtbar. Wenn Sie auf einen Typ in dieser Assembly von COM aus zugreifen
+// müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar
+// gemacht wird
+[assembly: Guid("f73bccb4-c2b8-4427-9e25-d6cb91d55482")]
---
+++ cscore-master/Samples/RecordWithSpecificFormat/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("37b5903a-13ba-4b28-a98b-34697339cdd0")]
